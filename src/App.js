d0f9c5fd285c1a31bef88a291dbb4e00694a08ab
course <rustamabduraximov140@gmail.com>
2024-07-10 11:12:56 +0500

    #8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { Register, Login, Main } from './components'
+import { Register, Login, Main, Navbar } from './components'
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path = '/' element={<Main/>} />
         <Route path = '/login' element={<Login/>}/>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Main} from './main/main'
 export {default as Login} from './login/login'
 export {default as Register} from './register/register'
+export {default as Navbar} from './navbar/navbar'
